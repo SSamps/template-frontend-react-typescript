9c409bfa8c5d3b6f4d0afbdc51d6b643b9f76e28
Simon Sampson <simonjsampson@gmail.com>
2021-04-26 21:52:29 +0100

    Set up very basic front end with landing and register form

---
+++ src/components/auth/Register.tsx
@@ -0,0 +1,85 @@
+import React, { Fragment, useState } from 'react';
+import { Link } from 'react-router-dom';
+
+const Register = () => {
+    const [formData, setFormData] = useState({
+        displayName: '',
+        email: '',
+        password: '',
+        password2: '',
+    });
+
+    const { displayName, email, password, password2 } = formData;
+
+    const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setFormData({ ...formData, [e.target.name]: e.target.value });
+    };
+
+    const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        if (password !== password2) {
+            console.log('Passwords do not match');
+        } else {
+            console.log('Success');
+        }
+    };
+
+    return (
+        <Fragment>
+            <h1 className='large text-primary'>Sign Up</h1>
+            <p className='lead'>
+                <i className='fas fa-user'></i> Create Your Account
+            </p>
+            <form className='form' onSubmit={(e) => onSubmit(e)}>
+                <div className='form-group'>
+                    <input
+                        type='text'
+                        placeholder='Display Name'
+                        name='displayName'
+                        value={displayName}
+                        onChange={(e) => onChange(e)}
+                        required
+                    />
+                </div>
+                <div className='form-group'>
+                    <input
+                        type='email'
+                        placeholder='Email Address'
+                        name='email'
+                        value={email}
+                        onChange={(e) => onChange(e)}
+                        required
+                    />
+                </div>
+                <div className='form-group'>
+                    <input
+                        type='password'
+                        placeholder='Password'
+                        name='password'
+                        minLength={8}
+                        value={password}
+                        onChange={(e) => onChange(e)}
+                        required
+                    />
+                </div>
+                <div className='form-group'>
+                    <input
+                        type='password'
+                        placeholder='Confirm Password'
+                        name='password2'
+                        minLength={8}
+                        value={password2}
+                        onChange={(e) => onChange(e)}
+                        required
+                    />
+                </div>
+                <input type='submit' className='btn btn-primary' value='Register' />
+            </form>
+            <p className='my-1'>
+                Already have an account? <Link to='/login'>Sign In</Link>
+            </p>
+        </Fragment>
+    );
+};
+
+export default Register;
